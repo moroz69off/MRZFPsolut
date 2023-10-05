77294a167bbbe63616f22578631af51ea363673e
Yuri Morozov <moroz69off@users.noreply.github.com>
2023-10-05 07:18:58 +0300

    Update Program.cs
    secret service

--- MRZFP/Program.cs
+++ MRZFP/Program.cs
@@ -5,7 +5,7 @@ using Telegram.Bot;
 //Console.WriteLine("Hello, World!");
 
 
-TelegramBotClient botClient = new TelegramBotClient("6565715841:AAEImiPVEnQ9fy7lE7ZDCkLvl2gi1zutmvg");
+TelegramBotClient botClient = new TelegramBotClient("token");
 
 Telegram.Bot.Types.User me = await botClient.GetMeAsync();
 
