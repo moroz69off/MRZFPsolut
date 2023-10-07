ba298ce309735eb4427711f6465a8e70c9f12ea4
moroz69off <moroz69off@gmail.com>
2023-10-07 09:31:44 +0300

    delete token

--- MRZFP/Program.cs
+++ MRZFP/Program.cs
@@ -1,17 +1,75 @@
-﻿using Telegram;
-using Telegram.Bot;
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 
 //Console.WriteLine("Hello, World!");
 
+const string token = "token";
 
-TelegramBotClient botClient = new TelegramBotClient("6565715841:AAEImiPVEnQ9fy7lE7ZDCkLvl2gi1zutmvg");
+TelegramBotClient botClient = new TelegramBotClient(token);
 
 Telegram.Bot.Types.User me = await botClient.GetMeAsync();
 
 
+using CancellationTokenSource cts = new();
 
-Console.WriteLine($"I am user {me.Id} and my name is {me.FirstName}.");
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new(){AllowedUpdates = Array.Empty<UpdateType>()};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    // Echo received message text
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "You said:\n" + messageText,
+        cancellationToken: cancellationToken);
+}
+
+
+
+//Console.WriteLine($"I am user {me.Id} and my name is {me.FirstName}.");
 
 Console.ReadKey(false);
 //________
+
+
+
+
+
+
+
+
